acb05fd6a24e02e592d3dd7a4b294c17323ed97c
Paul Ambrose <pambrose@mac.com>
2019-08-04 17:18:53 -0700

    Add verbose example of lambdas

--- src/main/kotlin/org/athenian/lambdas/0_SimpleExample.kt
+++ src/main/kotlin/org/athenian/lambdas/0_SimpleExample.kt
@@ -1,9 +1,13 @@
 package org.athenian.lambdas
 
 
-val constant: () -> Int = { 4 }
+// Lambda without params
+val constant1: () -> Int = fun(): Int { return 4 }
+val constant2: () -> Int = { 4 }
 
-val adder: (Int, Int) -> Int = { a: Int, b: Int -> a + b }
+// Lambda with params
+val adder1: (Int, Int) -> Int = fun(a: Int, b: Int): Int { return a + b }
+val adder2: (Int, Int) -> Int = { a: Int, b: Int -> a + b }
 
 fun sum(x: Int, y: Int): Int = x + y
 
@@ -11,11 +15,11 @@ fun twoIntFunc(x: Int, y: Int, block: (Int, Int) -> Int): Int = block(x, y)
 
 fun main() {
 
-    println(constant.invoke())
-    println(constant())
+    println(constant2.invoke())
+    println(constant2())
 
-    println(adder.invoke(4, 5))
-    println(adder(4, 5))
+    println(adder2.invoke(4, 5))
+    println(adder2(4, 5))
 
     println(twoIntFunc(5, 6) { x, y -> x * y })
 
